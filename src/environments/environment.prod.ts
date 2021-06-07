9cd41d696cecc0bbff5d893f020f7562f96afde3
Santhosh K <santhosh.k@corp.rapyder.com>
2021-06-07 16:09:20 +0530

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  getUsers: 'https://zp1fuzpqhg.execute-api.ap-south-1.amazonaws.com/tallyUser/dummy',
+  loginMockup: 'https://zp1fuzpqhg.execute-api.ap-south-1.amazonaws.com/tallyUser/loginmockup',
+  restartLaptop: 'https://zp1fuzpqhg.execute-api.ap-south-1.amazonaws.com/tallyUser/restartlaptop',
+  restartComputer: 'https://zp1fuzpqhg.execute-api.ap-south-1.amazonaws.com/tallyUser/restartcomputer'
+};
